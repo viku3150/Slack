e101eb27ca57a110aa9a9b67116bf354cd566844
Vikash <21vikashgupta@gmail.com>
2021-12-05 02:26:35 +0530

    updated

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA_c9L7lu-EEyMY2G_1ezFVl9W2MA-FSFg",
+  authDomain: "slack-clone-2a780.firebaseapp.com",
+  projectId: "slack-clone-2a780",
+  storageBucket: "slack-clone-2a780.appspot.com",
+  messagingSenderId: "52028081793",
+  appId: "1:52028081793:web:8b252d570024d1f2daaf80",
+};
