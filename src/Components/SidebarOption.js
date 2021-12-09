b06c935d19aa6a34aef263979d75063d367228ff
Vikash <21vikashgupta@gmail.com>
2021-12-09 21:41:27 +0530

    Sidebar redux integration

--- src/Components/SidebarOption.js
+++ src/Components/SidebarOption.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
-// import { getFirestore, collection } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
+import { useDispatch } from "react-redux";
+import { enterRoom } from "../features/appSlice";
 
-function SidebarOption({ Icon, title, addChannelOption }) {
+function SidebarOption({ Icon, title, addChannelOption, id }) {
+  const dispatch = useDispatch();
+
   const [channels] = useCollection(db.collection("rooms"));
-  console.log(channels);
   const addChannel = () => {
     const channelName = prompt("Please enter the Channel Name");
     if (channelName) {
@@ -15,7 +17,15 @@ function SidebarOption({ Icon, title, addChannelOption }) {
       });
     }
   };
-  const selectChannel = () => {};
+  const selectChannel = () => {
+    if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id,
+        })
+      );
+    }
+  };
 
   return (
     <SidebarOptionContainer
---
+++ src/features/appSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+export const appSlice = createSlice({
+  name: "counter3",
+  initialState: {
+    roomId: null,
+  },
+  reducers: {
+    enterRoom: (state, action) => {
+      state.roomId = action.payload.roomId;
+    },
+  },
+});
+
+export const { enterRoom } = appSlice.actions;
+
+export const selectRoomId = (state) => state.app.roomId;
+
+export default appSlice.reducer;
